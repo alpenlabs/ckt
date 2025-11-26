3b6058833ff1ae04c5d9846cbaa86c2cb18046ce
Trey Del Bonis <trey@alpenlabs.io>
2025-11-26 16:30:34 -0500

    refactor: convert test garble fn to use circuit worker system

---
+++ crates/runner-exec/src/worker.rs
@@ -0,0 +1,55 @@
+use ckt_fmtv5_types::v5::c::*;
+use ckt_runner_types::{CircuitTask, GateBlock};
+
+/// Proceses a circuit task by reading blocks out of a `ReaderV5c`.
+pub async fn process_task<T: CircuitTask>(
+    task_info: &T,
+    reader: &mut ReaderV5c,
+) -> anyhow::Result<T::Output> {
+    // Initialize the task and run through the circuit.
+    let mut task_state = task_info.initialize(reader.header())?;
+    if let Err(e) = process_task_inner(task_info, &mut task_state, reader).await {
+        task_info.on_abort(task_state);
+        return Err(e);
+    }
+
+    // Produce the output using the output indexes.
+    let output_wires = reader
+        .outputs()
+        .iter()
+        .map(|w| *w as u64)
+        .collect::<Vec<_>>();
+    let task_output = task_info.finish(task_state, &output_wires)?;
+
+    // TODO maybe do some other cleanup?
+
+    Ok(task_output)
+}
+
+async fn process_task_inner<T: CircuitTask>(
+    task_info: &T,
+    task_state: &mut T::State,
+    reader: &mut ReaderV5c,
+) -> anyhow::Result<()> {
+    let total_gates = reader.header().total_gates();
+
+    // Iterate over every chunk, keeping track of where we are.
+    let mut cur_block_idx = 0;
+    let mut total_gates_processed = 0;
+    while let Some(chunk) = reader.next_blocks_chunk().await? {
+        // Iterate over each block in the chunk and pass it to the task to do something with it.
+        for block in chunk.blocks_iter() {
+            let gates_in_block = get_block_num_gates(total_gates, cur_block_idx);
+            cur_block_idx += 1;
+
+            let safe_block = GateBlock::new(block, gates_in_block);
+            task_info.on_block(task_state, &safe_block)?;
+
+            total_gates_processed += gates_in_block;
+        }
+
+        task_info.on_after_chunk(task_state)?;
+    }
+
+    Ok(())
+}
--- crates/runner-types/src/traits.rs
+++ crates/runner-types/src/traits.rs
@@ -1,7 +1,45 @@
 //! Runner task traits.
 
-pub trait CircuitTask {
-    type Error;
+use std::error::Error;
 
-    fn visit_chunk(&mut self, chunk: &Chunk);
+use ckt_fmtv5_types::v5::c::HeaderV5c;
+
+use crate::block::GateBlock;
+
+/// Represents a task that walks over processing blocks of gates from a circuit.
+pub trait CircuitTask {
+    /// Error type for whatever errors might happen.
+    type Error: Error + Sync + Send + 'static;
+
+    /// Running task state.
+    type State;
+
+    /// Output produced by the task.
+    type Output;
+
+    /// Visits the header to initialize whatever needs to be and produce a
+    /// running task state.
+    ///
+    /// This is called first and once.
+    fn initialize(&self, header: &HeaderV5c) -> Result<Self::State, Self::Error>;
+
+    /// Visits a block of gates for processing.  This modifies the passed task
+    /// state as needed by the task.
+    fn on_block(&self, state: &mut Self::State, block: &GateBlock<'_>) -> Result<(), Self::Error>;
+
+    /// Called after each block in a chunk has been processed.
+    fn on_after_chunk(&self, state: &mut Self::State) -> Result<(), Self::Error>;
+
+    /// Called at the end after we've processed all the chunks, consuming the
+    /// final task state to produce an output.
+    fn finish(
+        &self,
+        state: Self::State,
+        output_wire_idxs: &[u64],
+    ) -> Result<Self::Output, Self::Error>;
+
+    /// Called if an error occurs to be able to perform graceful cleanup.
+    ///
+    /// This is not called if an error occurs in `finish`.
+    fn on_abort(&self, state: Self::State);
 }
