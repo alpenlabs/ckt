4476d7ccf745924644288b67e2e61723dfc1a4a1
Trey Del Bonis <trey@alpenlabs.io>
2025-12-03 16:21:44 -0500

    lint: lies

--- crates/fmtv5-types/src/lib.rs
+++ crates/fmtv5-types/src/lib.rs
@@ -1,7 +1,7 @@
 //! CKT - High-performance Boolean circuit format library
 
 #![expect(missing_docs, reason = "please let me merge")]
-#![expect(unreachable_pub, reason = "pleeeeease let me merge")]
+#![allow(unreachable_pub, reason = "pleeeeease let me merge")] // clippy is wrong about this being unfulfilled
 #![expect(missing_debug_implementations, reason = "merge pls")]
 
 #[cfg(feature = "v5")]
