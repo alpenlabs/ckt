cabc82cbbded4a053f25df867a9b939a858bfe2e
Nakul Khambhati <nakulkhambhati@g.ucla.edu>
2025-12-12 15:03:01 +0530

    fixed clippy error about long para docstring

--- crates/gobble/src/x86_64/mod.rs
+++ crates/gobble/src/x86_64/mod.rs
@@ -178,7 +178,9 @@ pub fn encode(input: Vec<bool>, false_input_labels: Vec<Label>, delta: Label) ->
     selected_input_labels
 }
 
-/// Tweakable circular correlation robust (TCCR) hash function instantiated via fixed-key AES as H(x, tweak) = AES(AES(x) ⊕ tweak) ⊕ AES(x)
+/// TCCR hash function via fixed-key AES: H(x, tweak) = AES(AES(x) ⊕ tweak) ⊕ AES(x).
+///
+/// Tweakable circular correlation robust (TCCR) hash function.
 /// Referenced from Section 7.4 of [GKWY20] <https://eprint.iacr.org/2019/074>.
 ///
 /// # Safety
